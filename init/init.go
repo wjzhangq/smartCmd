b252db5ade3180a53d53243cdd00f477f4399f7d
wjzhangq <wjzhangq@126.com>
2026-03-08 11:48:34 +0800

    替换为sc 和 scc

--- init/init.go
+++ init/init.go
@@ -20,8 +20,8 @@ func GenerateInitScript(info *sysinfo.SystemInfo) string {
 // generatePowerShellScript 生成 PowerShell 初始化脚本
 func generatePowerShellScript(info *sysinfo.SystemInfo) string {
 	return fmt.Sprintf(`# smartCmd init output for PowerShell
-function global:? { & '%s' parse $args }
-function global:?? { & '%s' parseAndRun $args }
+function global:sc { & '%s' parse $args }
+function global:scc { & '%s' parseAndRun $args }
 $env:_smartCmd = '%s'
 `, info.BinPath, info.BinPath, info.ToEnvString())
 }
@@ -29,8 +29,8 @@ $env:_smartCmd = '%s'
 // generateFishScript 生成 Fish Shell 初始化脚本
 func generateFishScript(info *sysinfo.SystemInfo) string {
 	return fmt.Sprintf(`# smartCmd init output for fish
-function ?; %s parse $argv; end
-function ??; %s parseAndRun $argv; end
+function sc; %s parse $argv; end
+function scc; %s parseAndRun $argv; end
 set -x _smartCmd '%s'
 `, info.BinPath, info.BinPath, info.ToEnvString())
 }
@@ -38,8 +38,8 @@ set -x _smartCmd '%s'
 // generatePosixScript 生成 POSIX 兼容脚本 (bash/zsh/sh)
 func generatePosixScript(info *sysinfo.SystemInfo) string {
 	return fmt.Sprintf(`# smartCmd init output for %s
-alias ?='%s parse'
-alias ??='%s parseAndRun'
+alias sc='%s parse'
+alias scc='%s parseAndRun'
 export _smartCmd='%s'
 `, info.Shell, info.BinPath, info.BinPath, info.ToEnvString())
 }
